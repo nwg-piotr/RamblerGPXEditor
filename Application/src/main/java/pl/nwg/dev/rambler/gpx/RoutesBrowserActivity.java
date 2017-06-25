a7adf5d699956106fb64d14f9c2a441a3e4ee3e1
nwg-piotr <nwg.piotr@gmail.com>
2017-06-25 03:29:49 +0200

    POI layer added

--- Application/src/main/java/pl/nwg/dev/rambler/gpx/RoutesBrowserActivity.java
+++ Application/src/main/java/pl/nwg/dev/rambler/gpx/RoutesBrowserActivity.java
@@ -27,6 +27,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -50,7 +51,11 @@ import com.google.android.gms.location.LocationServices;
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.events.MapEventsReceiver;
+import org.osmdroid.events.MapListener;
+import org.osmdroid.events.ScrollEvent;
+import org.osmdroid.events.ZoomEvent;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.MapEventsOverlay;
@@ -73,6 +78,7 @@ import java.util.Map;
 import pt.karambola.commons.collections.ListUtils;
 import pt.karambola.geo.Units;
 import pt.karambola.gpx.beans.Gpx;
+import pt.karambola.gpx.beans.Point;
 import pt.karambola.gpx.beans.Route;
 import pt.karambola.gpx.beans.RoutePoint;
 import pt.karambola.gpx.beans.Track;
@@ -230,6 +236,7 @@ public class RoutesBrowserActivity extends Utils
             public boolean singleTapConfirmedHelper(GeoPoint p) {
 
                 Data.sSelectedRouteIdx = null;
+                mMapDragged = true;
                 refreshMap(false);
 
                 return false;
@@ -243,6 +250,24 @@ public class RoutesBrowserActivity extends Utils
         };
 
         restoreMapPosition();
+
+        mMapView.setMapListener(new MapListener() {
+            @Override
+            public boolean onScroll(ScrollEvent event) {
+
+                mMapDragged = true;
+                //refreshMap();
+                return false;
+            }
+
+            @Override
+            public boolean onZoom(ZoomEvent event) {
+
+                mMapDragged = true;
+
+                return false;
+            }
+        });
 
         setUpButtons();
         setButtonsState();
@@ -454,12 +479,75 @@ public class RoutesBrowserActivity extends Utils
             mMapView.zoomToBoundingBox(findBoundingBox(mAllGeopoints), false);
         }
 
+        if (showPoi) {
+            drawPoi();
+        }
+
         mMapView.invalidate();
         setButtonsState();
     }
 
     private void refreshMap() {
         refreshMap(true);
+    }
+
+    private void drawPoi() {
+
+        BoundingBox mMapViewBoundingBox = mMapView.getBoundingBox();
+
+        Data.sFilteredPoi = ListUtils.filter(Data.sPoiGpx.getPoints(), Data.sViewPoiFilter);
+
+        /*
+         * Let's assign a color to each existing POI type
+         */
+        List<String> wptTypes = GpxUtils.getDistinctPointTypes(Data.sFilteredPoi);
+
+        Map<String,Integer>	wptTypeColourMap = new HashMap<>();
+        int colourIdx = 0;
+        for (String wptType: wptTypes) {
+            wptTypeColourMap.put(wptType, typeColors[colourIdx++ % N_COLOURS]);
+        }
+
+        for (Point poi : Data.sFilteredPoi) {
+
+            GeoPoint markerPosition = new GeoPoint(poi.getLatitude(), poi.getLongitude());
+
+            String displayName;
+            if(poi.getName() != null && !poi.getName().isEmpty()) {
+                displayName = poi.getName();
+            } else {
+                displayName = String.valueOf(Data.sFilteredPoi.indexOf(poi));
+            }
+
+            /*
+             * Use the color from the map if the POI has a type defined.
+             * If not - paint in grey.
+             */
+            int color;
+            if (poi.getType() == null) {
+                color = Color.parseColor("#999999");
+            } else {
+                color = wptTypeColourMap.get(poi.getType());
+            }
+            Drawable icon = new BitmapDrawable(getResources(), makeMarkerBitmap(this, displayName, color));
+
+            Marker marker = new Marker(mMapView);
+            marker.setPosition(markerPosition);
+            marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            marker.setDraggable(false);
+            marker.setIcon(icon);
+
+            marker.setOnMarkerClickListener(new Marker.OnMarkerClickListener() {
+                @Override
+                public boolean onMarkerClick(Marker marker, MapView mapView) {
+                    return false;
+                }
+            });
+
+            if (mMapViewBoundingBox.contains(markerPosition)) {
+                mMapView.getOverlays().add(marker);
+            }
+        }
     }
 
     private void setUpButtons() {
@@ -486,6 +574,8 @@ public class RoutesBrowserActivity extends Utils
                 } else {
                     Toast.makeText(getApplicationContext(), getString(R.string.no_routes_in_view), Toast.LENGTH_SHORT).show();
                 }
+                mMapDragged = true;
+                refreshMap();
             }
         });
 
@@ -503,6 +593,7 @@ public class RoutesBrowserActivity extends Utils
                         Data.sSelectedRouteIdx = 0;
                     }
                 }
+                mMapDragged = true;
                 refreshMap();
             }
         });
@@ -520,6 +611,7 @@ public class RoutesBrowserActivity extends Utils
                         Data.sSelectedRouteIdx = mFilteredRoutesNumber -1;
                     }
                 }
+                mMapDragged = true;
                 refreshMap();
             }
         });
@@ -616,6 +708,7 @@ public class RoutesBrowserActivity extends Utils
 
         mGoogleApiClient.connect();
         restoreMapPosition();
+        loadSettings();
         refreshMap(false);
     }
 
@@ -721,6 +814,8 @@ public class RoutesBrowserActivity extends Utils
         menu.findItem(R.id.routes_simplify_selected).setEnabled(Data.sSelectedRouteIdx != null);
         menu.findItem(R.id.routes_clear).setEnabled(Data.sRoutesGpx.getRoutes().size() > 0);
         menu.findItem(R.id.export).setEnabled(Data.sRoutesGpx.getRoutes().size() > 0);
+
+        menu.findItem(R.id.show_poi).setChecked(showPoi);
 
         return super.onPrepareOptionsMenu(menu);
     }
@@ -849,6 +944,13 @@ public class RoutesBrowserActivity extends Utils
                 filePickerAction = SAVE_MULTIPLE_ROUTES;
                 displayExportMultipleDialog();
                 return true;
+
+            case R.id.show_poi:
+
+                showPoi = !showPoi;
+                saveSettings();
+                refreshMap();
+                return true;
         }
 
         // Activate the navigation drawer toggle
@@ -872,6 +974,7 @@ public class RoutesBrowserActivity extends Utils
 
                 if (drawerIdxToRouteIdx != null && !drawerIdxToRouteIdx.isEmpty()) {
                     Data.sSelectedRouteIdx = drawerIdxToRouteIdx.get(position);
+                    mMapDragged = true;
                     refreshMap();
                 }
             }
@@ -1058,6 +1161,7 @@ public class RoutesBrowserActivity extends Utils
                         // clear current filters
                         Data.sViewRouteFilter = new RouteFilter();
                         Data.sSelectedRouteIdx = null;
+                        mMapDragged = true;
                         refreshMap();
 
                     }
@@ -1122,6 +1226,7 @@ public class RoutesBrowserActivity extends Utils
 
                         Data.sSelectedRouteIdx = null;
 
+                        mMapDragged = true;
                         refreshMap(true);
 
                     }
@@ -1983,5 +2088,24 @@ public class RoutesBrowserActivity extends Utils
                 Toast.makeText(getApplicationContext(), getResources().getString(R.string.failed_writing_gpx), Toast.LENGTH_LONG).show();
             }
         }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+
+            case MotionEvent.ACTION_UP:
+
+                if (mMapDragged) {
+
+                    mMapDragged = false;
+
+                    refreshMap(false);
+                }
+                break;
+        }
+        return true;
+
     }
 }
